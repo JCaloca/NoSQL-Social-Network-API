3d35b21f3e0193e74ff563c673bf41cc7704cda9
JCaloca <jacobcaloca1@gmail.com>
2022-11-26 14:54:35 -0800

    created route for getting user by ID

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,4 +23,18 @@ module.exports = {
         res.status(500).json(err);
       });
   },
+
+  // Get User by ID
+  getUserById(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .populate({ path: "friends", select: "-__v" })
+      .populate({ path: "thoughts", select: "-__v" })
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
 
 // importing user controllers
-const { createUser, getUsers } = require("../../controllers/userController");
+const {
+  createUser,
+  getUsers,
+  getUserById,
+} = require("../../controllers/userController");
 
 // route api/users to get all users and create a user
 router.route("/").get(getUsers).post(createUser);
+
+// api/users/:userId to get user by ID
+router.route("/:userId").get(getUserById);
 
 module.exports = router;
